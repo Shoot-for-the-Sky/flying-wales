04fe12249438390a5ce40eaf97f582774112e582
itamarcasspi <itamarcasspi@gmail.com>
2023-06-11 17:46:13 +0300

    (#6)

---
+++ Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape pressed");
+            if(gameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        LoadMenu();
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Load Menu");
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+
+}   
