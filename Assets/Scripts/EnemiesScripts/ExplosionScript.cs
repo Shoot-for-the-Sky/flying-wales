33fed54f7430dbd0e92abf27171405e43a9968dc
Kfir Goldfarb <kggold4@gmail.com>
2023-06-14 20:38:48 +0300

    UI fixes * Fix score icon * Fix audio loop * Improve explosions * Order project file

--- Assets/Scripts/EnemiesScripts/ExplosionScript.cs
+++ Assets/Scripts/EnemiesScripts/ExplosionScript.cs
@@ -5,10 +5,25 @@ public class ExplosionScript : MonoBehaviour
     [SerializeField] private GameObject explosionPrefab;
     [SerializeField] private float timeToAppear;
     [SerializeField] private float movingSpeed;
+    [SerializeField] private float minRandomScale;
+    [SerializeField] private float maxRandomScale;
+    [SerializeField] private float rangeRandomPositionX;
+    [SerializeField] private float rangeRandomPositionY;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Scale
+        float randomScale = UtilFunctions.GetRandomDoubleInRange(minRandomScale, maxRandomScale);
+        transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+
+        // Position
+        float randomAxisX = UtilFunctions.GetRandomDoubleInRange(-rangeRandomPositionX, rangeRandomPositionX);
+        float randomAxisY = UtilFunctions.GetRandomDoubleInRange(-rangeRandomPositionY, rangeRandomPositionY);
+        Vector3 newPosition = new Vector3(transform.position.x + randomAxisX, transform.position.y + randomAxisY, transform.position.z);
+        transform.position = newPosition;
+        
+        // Destroy
         FunctionTimer.Create(SelfDestroy, timeToAppear);
     }
 
